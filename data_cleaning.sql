27c14dbc90a65e6f50a94a0b1b6e4dda007a2122
Phan Ngọc Vương <vuongwngoc2k1@gmail.com>
2026-06-09 22:04:15 +0700

    Update comments in data_cleaning.sql
    Refactor comments for clarity and consistency in data cleaning SQL script.

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -12,7 +12,7 @@ FROM world_layoffs.layoffs;
 
 
 
--- first thing we want to do is create a staging table. This is the one we will work in and clean the data. We want a table with the raw data in case something happens
+
 CREATE TABLE world_layoffs.layoffs_staging 
 LIKE world_layoffs.layoffs;
 
@@ -20,7 +20,7 @@ INSERT layoffs_staging
 SELECT * FROM world_layoffs.layoffs;
 
 
--- now when we are data cleaning we usually follow a few steps
+-- now when i am data cleaning i usually follow a few steps
 -- 1. check for duplicates and remove any
 -- 2. standardize data and fix errors
 -- 3. Look at null values and see what 
@@ -63,7 +63,7 @@ SELECT *
 FROM world_layoffs.layoffs_staging
 WHERE company = 'Oda'
 ;
--- it looks like these are all legitimate entries and shouldn't be deleted. We need to really look at every single row to be accurate
+-- it looks like these are all legitimate entries and shouldn't be deleted. I need to really look at every single row to be accurate
 
 -- these are our real duplicates 
 SELECT *
@@ -78,9 +78,9 @@ FROM (
 WHERE 
 	row_num > 1;
 
--- these are the ones we want to delete where the row number is > 1 or 2or greater essentially
+-- these are the ones i want to delete where the row number is > 1 or 2or greater essentially
 
--- now you may want to write it like this:
+-- now i may want to write it like this:
 WITH DELETE_CTE AS 
 (
 SELECT *
@@ -112,7 +112,6 @@ WHERE (company, location, industry, total_laid_off, percentage_laid_off, `date`,
 ) AND row_num > 1;
 
 -- one solution, which I think is a good one. Is to create a new column and add those row numbers in. Then delete where row numbers are over 2, then delete that column
--- so let's do it!!
 
 ALTER TABLE world_layoffs.layoffs_staging ADD row_num INT;
 
@@ -160,7 +159,7 @@ SELECT `company`,
 	FROM 
 		world_layoffs.layoffs_staging;
 
--- now that we have this we can delete rows were row_num is greater than 2
+-- now that i have this i can delete rows ire row_num is greater than 2
 
 DELETE FROM world_layoffs.layoffs_staging2
 WHERE row_num >= 2;
@@ -176,7 +175,7 @@ WHERE row_num >= 2;
 SELECT * 
 FROM world_layoffs.layoffs_staging2;
 
--- if we look at industry it looks like we have some null and empty rows, let's take a look at these
+-- if i look at industry it looks like i have some null and empty rows, let's take a look at these
 SELECT DISTINCT industry
 FROM world_layoffs.layoffs_staging2
 ORDER BY industry;
@@ -197,16 +196,16 @@ FROM world_layoffs.layoffs_staging2
 WHERE company LIKE 'airbnb%';
 
 -- it looks like airbnb is a travel, but this one just isn't populated.
--- I'm sure it's the same for the others. What we can do is
+-- I'm sure it's the same for the others. What i can do is
 -- write a query that if there is another row with the same company name, it will update it to the non-null industry values
--- makes it easy so if there were thousands we wouldn't have to manually check them all
+-- makes it easy so if there ire thousands i wouldn't have to manually check them all
 
--- we should set the blanks to nulls since those are typically easier to work with
+-- i should set the blanks to nulls since those are typically easier to work with
 UPDATE world_layoffs.layoffs_staging2
 SET industry = NULL
 WHERE industry = '';
 
--- now if we check those are all null
+-- now if i check those are all null
 
 SELECT *
 FROM world_layoffs.layoffs_staging2
@@ -214,7 +213,7 @@ WHERE industry IS NULL
 OR industry = ''
 ORDER BY industry;
 
--- now we need to populate those nulls if possible
+-- now i need to populate those nulls if possible
 
 UPDATE layoffs_staging2 t1
 JOIN layoffs_staging2 t2
@@ -223,7 +222,7 @@ SET t1.industry = t2.industry
 WHERE t1.industry IS NULL
 AND t2.industry IS NOT NULL;
 
--- and if we check it looks like Bally's was the only one without a populated row to populate this null values
+-- and if i check it looks like Bally's was the only one without a populated row to populate this null values
 SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE industry IS NULL 
@@ -232,7 +231,7 @@ ORDER BY industry;
 
 -- ---------------------------------------------------
 
--- I also noticed the Crypto has multiple different variations. We need to standardize that - let's say all to Crypto
+-- I also noticed the Crypto has multiple different variations. i need to standardize that - let's say all to Crypto
 SELECT DISTINCT industry
 FROM world_layoffs.layoffs_staging2
 ORDER BY industry;
@@ -247,12 +246,12 @@ FROM world_layoffs.layoffs_staging2
 ORDER BY industry;
 
 -- --------------------------------------------------
--- we also need to look at 
+-- i also need to look at 
 
 SELECT *
 FROM world_layoffs.layoffs_staging2;
 
--- everything looks good except apparently we have some "United States" and some "United States." with a period at the end. Let's standardize this.
+-- everything looks good except apparently i have some "United States" and some "United States." with a period at the end. Let's standardize this.
 SELECT DISTINCT country
 FROM world_layoffs.layoffs_staging2
 ORDER BY country;
@@ -260,7 +259,7 @@ ORDER BY country;
 UPDATE layoffs_staging2
 SET country = TRIM(TRAILING '.' FROM country);
 
--- now if we run this again it is fixed
+-- now if i run this again it is fixed
 SELECT DISTINCT country
 FROM world_layoffs.layoffs_staging2
 ORDER BY country;
@@ -270,11 +269,11 @@ ORDER BY country;
 SELECT *
 FROM world_layoffs.layoffs_staging2;
 
--- we can use str to date to update this field
+-- i can use str to date to update this field
 UPDATE layoffs_staging2
 SET `date` = STR_TO_DATE(`date`, '%m/%d/%Y');
 
--- now we can convert the data type properly
+-- now i can convert the data type properly
 ALTER TABLE layoffs_staging2
 MODIFY COLUMN `date` DATE;
 
@@ -296,7 +295,7 @@ FROM world_layoffs.layoffs_staging2;
 
 
 
--- 4. remove any columns and rows we need to
+-- 4. remove any columns and rows i need to
 
 SELECT *
 FROM world_layoffs.layoffs_staging2
@@ -308,7 +307,7 @@ FROM world_layoffs.layoffs_staging2
 WHERE total_laid_off IS NULL
 AND percentage_laid_off IS NULL;
 
--- Delete Useless data we can't really use
+-- Delete Useless data i can't really use
 DELETE FROM world_layoffs.layoffs_staging2
 WHERE total_laid_off IS NULL
 AND percentage_laid_off IS NULL;
